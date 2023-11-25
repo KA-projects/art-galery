d889abc8497ec8f7f767718b530bbb9f8cc4105b
EdwardPierce <122209044+EdwardPierce@users.noreply.github.com>
2023-11-25 13:06:33 +0600

    added navbar component

--- src/components/home/navbar.vue
+++ src/components/home/navbar.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import Favourtires from "../svg/favourtires.vue";
+</script>
+
 <template>
-  <div class="bg-black w-full h-16">
-    <div class="text-white">fwefewfwe</div>
+  <div class="bg-black w-full h-[72px]">
+    <div class="text-white px-[18px] py-5">
+      <div class="flex justify-between items-center">
+        <div>
+          <img src="../../assets/logo.svg" alt="logo" />
+        </div>
+
+        <div><Favourtires /></div>
+      </div>
+    </div>
   </div>
 </template>
