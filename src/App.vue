7ea0485638893bb9709981bf94b02af88f952e70
EdwardPierce <122209044+EdwardPierce@users.noreply.github.com>
2023-11-25 17:01:21 +0600

    added gallery component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from "./components/home/navbar.vue";
 import Search from "./components/home/Search.vue";
+import Galery from "./components/home/Galery.vue";
 </script>
 
 <template>
   <Navbar />
   <Search />
+  <Galery />
 </template>
---
+++ src/components/home/Galery.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+type UnsplashApi = {
+  urls: {
+    raw: string;
+    regular: string;
+    full: string;
+    small: string;
+    thumb: string;
+  };
+};
+
+const data = ref<UnsplashApi[]>([]);
+
+const fetchData = async () => {
+  const res = await fetch("https://api.unsplash.com/photos/random?count=8", {
+    method: "GET",
+
+    headers: {
+      "Accept-Version": "v1",
+      Authorization: `Client-ID ${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`,
+    },
+  });
+
+  data.value = await res.json();
+  console.log(data.value);
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+
+<template>
+  <div class="mt-[50px]">
+    <div class="px-5">
+      <div class="grid grid-cols-1 gap-5">
+        <img
+          v-for="photo in data"
+          class="rounded-lg object-cover max-w-full w-full h-[311px]"
+          :src="photo.urls.small"
+          alt="one"
+        />
+      </div>
+    </div>
+  </div>
+</template>
